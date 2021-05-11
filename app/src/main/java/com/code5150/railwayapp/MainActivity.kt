f165ef662ffd0161a2c3323d231c8abf720779b6
Code5150 <f>
2021-05-11 18:33:57 +0400

    Начал делать логин приложения

--- app/src/main/java/com/code5150/railwayapp/MainActivity.kt
+++ app/src/main/java/com/code5150/railwayapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.code5150.railwayapp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.code5150.railwayapp.databinding.ActivityLoginBinding
+import com.code5150.railwayapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
--- app/src/main/java/com/code5150/railwayapp/network/ApiInterface.kt
+++ app/src/main/java/com/code5150/railwayapp/network/ApiInterface.kt
@@ -1,7 +1,11 @@
 package com.code5150.railwayapp.network
 
 import com.google.gson.GsonBuilder
+import okhttp3.Cookie
+import okhttp3.CookieJar
+import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Field
@@ -15,22 +19,40 @@ interface ApiInterface {
     suspend fun authorize(
         @Field("username") username: String,
         @Field("password") password: String
-    )
+    ): Boolean
 
     companion object {
         private const val API_URL = "http://localhost:8080/api/v1/"
-        operator fun invoke(): ApiInterface {
-            val gson = GsonBuilder().create()
-            return Retrofit.Builder()
-                .baseUrl(API_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .client(
-                    OkHttpClient().newBuilder()
-                        //.cookieJar()
-                        .build()
-                )
-                .build()
-                .create(ApiInterface::class.java)
+        private const val AUTH = "auth"
+
+        private val client = OkHttpClient().newBuilder()
+            .cookieJar(SessionCookieJar())
+            .build()
+        private val interfaceInstance = Retrofit.Builder()
+            .baseUrl(API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(ApiInterface::class.java)
+
+        operator fun invoke(): ApiInterface = interfaceInstance
+
+        private class SessionCookieJar: CookieJar{
+            private var cookies: MutableList<Cookie>? = null
+
+            override fun saveFromResponse(url: HttpUrl, cookies: MutableList<Cookie>) {
+                if (url.encodedPath().endsWith(AUTH)){
+                    this.cookies = cookies
+                }
+            }
+
+            override fun loadForRequest(url: HttpUrl): MutableList<Cookie> {
+                cookies?.let {
+                    if (!url.encodedPath().endsWith(AUTH)) return it
+                }
+                return emptyList<Cookie>().toMutableList()
+            }
+
         }
     }
 }
